6c8460db66155a12bca7a53174f0543852a4ea96
Nuri1977 <nurilacka@gmail.com>
2022-02-07 20:49:13 +0100

    Add styles for calculator

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="Calculator">
+      <div className="calculator">
         <Calculator />
       </div>
     );
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -9,27 +9,27 @@ class Calculator extends Component {
 
   render() {
     return (
-      <div id="calc-container" className="d-flex col">
-        <div id="calc-results">0</div>
+      <div className="calc-container">
+        <div className="span-four">0</div>
         <button type="button">AC</button>
         <button type="button">+/-</button>
         <button type="button">%</button>
-        <button type="button">÷</button>
+        <button type="button" className="orange">÷</button>
         <button type="button">7</button>
         <button type="button">8</button>
         <button type="button">9</button>
-        <button type="button">x</button>
+        <button type="button" className="orange">x</button>
         <button type="button">4</button>
         <button type="button">5</button>
         <button type="button">6</button>
-        <button type="button">-</button>
+        <button type="button" className="orange">-</button>
         <button type="button">1</button>
         <button type="button">2</button>
         <button type="button">3</button>
-        <button type="button">+</button>
-        <button type="button">0</button>
+        <button type="button" className="orange">+</button>
+        <button type="button" className="span-two">0</button>
         <button type="button">.</button>
-        <button type="button">=</button>
+        <button type="button" className="orange">=</button>
       </div>
     );
   }
