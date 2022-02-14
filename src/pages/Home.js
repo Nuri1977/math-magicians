7ec9cdb1515c4d0c6242f61d05554cc373c3b99e
Nuri1977 <nurilacka@gmail.com>
2022-02-14 12:37:30 +0100

    Add routing for 3 pages and markup

---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to our page!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Libero voluptatibus, recusandae, temporibus dignissimos
+        ullam porro earum repellendus explicabo
+        esse officiis ratione nulla vero
+        aliquam nisi molestias placeat, duci
+        mus quos nobis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Libero voluptatibus, recusandae, temporibus dignissimos
+        ullam porro earum repellendus explicabo
+        esse officiis ratione nulla vero
+        aliquam nisi molestias placeat, duci
+        mus quos nobis.
+      </p>
+    </div>
+  );
+}
---
+++ src/pages/Quote.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Quote() {
+  return (
+    <div>
+      <h1>
+        Mathematics is not about numbers, equations, computations, or algorithms:
+        it is about understanding. -Qilliam Paul Th
+      </h1>
+    </div>
+  );
+}
